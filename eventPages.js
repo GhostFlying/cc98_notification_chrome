353eceed64e54c3afcc2bb7b11e74a1f7993bd16
GhostFlying <ghostflying@gmail.com>
2014-03-12 20:17:19 +0800

    prepare for ver 0.1

--- eventPages.js
+++ eventPages.js
@@ -1,15 +1,26 @@
-﻿/*document.addEventListener('DOMContentLoaded', function () {
-  console.log("loaded");
-  chrome.alarms.create('refresh', {periodInMinutes: 0.1});
-  chrome.alarms.onAlarm.addListener(onAlarm);
-});*/
-init();
+﻿init();
 var MESSAGE_LIST_URL = "http://www.cc98.org/usersms.asp?action=inbox";
 var MESSAGE_CONTENT_URL = "http://www.cc98.org/messanger.asp?action=read&id=";
+var MESSAGE_INBOX_URL = "http://www.cc98.org/usersms.asp?action=inbox";
+
+var lastShowedMessageId;
+
 function init(){
 	console.log ('Init start.');
 	chrome.alarms.onAlarm.addListener(onAlarm);
-	chrome.alarms.create('refresh', {periodInMinutes: 1});
+	chrome.alarms.create('refresh', {periodInMinutes: 5});
+	chrome.browserAction.onClicked.addListener(goToInbox);
+
+	lastShowedMessageId = localStorage.getItem('lastShowedMessageId');
+
+	if (lastShowedMessageId == null) {
+		console.log ("No lastShowedMessageId record.");
+		lastShowedMessageId = 0;
+	}
+}
+
+function goToInbox(){
+	chrome.tabs.create({url:MESSAGE_INBOX_URL,active:true}, function(){})
 }
 
 
@@ -59,43 +70,55 @@ function getUnreedNum(){
 	htmlobj=$.ajax({url:MESSAGE_LIST_URL,async:false});
 	pmListHtml = htmlobj.responseText;
 	indexOfUnreed = pmListHtml.indexOf('条未读消息');
-	console.log (indexOfUnreed);
+	//console.log (indexOfUnreed);
 	if (indexOfUnreed > 0 ) {
 		unreedNum = parseInt(pmListHtml.substr(indexOfUnreed - 1, 1));
 		console.log('Notify some unreed messages. Number: ' + unreedNum);
+		chrome.browserAction.setBadgeText({text:unreedNum.toString()});
 		onUnreedDetected(unreedNum, pmListHtml);
+	}
+	else {
+		chrome.browserAction.setBadgeText({text:""});
 	}
 }
 
 function onUnreedDetected (unreedNum, pmListHtml){
 	console.log ('onUnreedDetected start.');
 	messageIdList = pmListHtml.match(/name=id value=\d+/g);
-	console.log (messageIdList);
+	//console.log (messageIdList);
 	messageTitles = pmListHtml.match(/\n\s{4}>.+(?=<\/a><\/td>)/g);
-	messageSenders = pmListHtml.match(/target=_blank>.+(?=<\/a>)/g);
-	messagesList = [];
-	//console.log(messageSenders);
-	for (i = 0; i < unreedNum; i++){
-		messageTitle = messageTitles[i].substr(5).replace(/&nbsp;/g,' ');
-		messageSender = messageSenders[i*2].substr(14);
-		//console.log (messageSender);
-		opt = {
-			type:"basic",
-			title: '收到一条来自  ' + messageSender + '  的新消息',
-			message: '标题：' +  messageTitle + '\n     点击查看详细内容',
-			iconUrl: "http://www.cc98.org/favicon.ico"
+	messageSenders = pmListHtml.match(/target=_blank>.+(?=<\/a>)/g);	
+	//messagesList = [];
+	//console.log(messageTimes);
+	for (i = unreedNum - 1; i > -1; i--){
+		messageId = messageIdList[i].substr(14);
+		if (messageId > lastShowedMessageId) {
+			console.log ('New Message.');
+			messageTitle = messageTitles[i].substr(5).replace(/&nbsp;/g,' ');
+			messageSender = messageSenders[i*2].substr(14);
+			opt = {
+				type:"basic",
+				title: '收到一条来自  ' + messageSender + '  的新消息',
+				message: '标题：' +  messageTitle + '\n     点击查看详细内容',
+				iconUrl: "http://www.cc98.org/favicon.ico"
+			}
+			console.log ('start notification' + messageId);
+			chrome.notifications.create('message' + messageId, opt, function(){});
+			chrome.notifications.onClicked.addListener(onNotificationClicked);
+			lastShowedMessageId = messageId;
 		}
-		console.log ('start notification' + messageIdList[i].substr(14));
-		chrome.notifications.create('message' + messageIdList[i].substr(14), opt, function(){});
-		chrome.notifications.onClicked.addListener(onNotificationClicked);
-		messageItem = {
+		else {
+			console.log ('Old Message.');
+		}
+		
+/*		messageItem = {
 			sender:"",
 			title:""
 		}
 		messageItem.sender = messageSender;
 		messageItem.title = messageTitle;
 		messagesList.push(messageItem);
-		console.log (messagesList);
+		console.log (messagesList);*/
 		/*processUnreedId = parseInt(messageIdList[i].substr(14));
 		console.log(processUnreedId);*/
 /*		messageContentHtml = $.ajax({url:MESSAGE_CONTENT_URL + processUnreedId,async:false}).responseText;
@@ -114,11 +137,13 @@ function onUnreedDetected (unreedNum, pmListHtml){
 		chrome.notifications.create('', opt, function(){});*/
 
 	}
-	localStorage.setItem('messages', JSON.stringify(messagesList));
+	localStorage.setItem('lastShowedMessageId', lastShowedMessageId);
+	//localStorage.setItem('messages', JSON.stringify(messagesList));
 
 }
 
 function onNotificationClicked (notificationId){
 	console.log('notification' + notificationId + 'clicked.');
+	chrome.tabs.create({url: MESSAGE_CONTENT_URL + notificationId.substr(7),active:true}, function(){});
 
 }
