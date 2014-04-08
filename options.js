7d6afd205a0bc314ef335646655426e18f3e1635
GhostFlying <ghostflying@gmail.com>
2014-04-08 15:17:06 +0800

    add storage step 1

--- options.js
+++ options.js
@@ -1,14 +1,40 @@
+var cookieNow;
+
 function getUserNow(){
   console.log("getUserNow");
-  chrome.cookies.get({url:"http://www.cc98.org", name:"aspsky"}, function (cookie){
-    console.log(cookie);
+  chrome.cookies.get({url:"http://www.cc98.org", name:"aspsky"}, function (cookie){    
     if (cookie == null){
-
+      cookieNow = null;
+      console.log('Not log in.');
     }
     else {
-      
+      console.log(cookie);
+      cookieNow = cookie;
+      setUserNow(cookie);
     }
   });
 }
 
+function setUserNow(cookie){
+  username = cookie.value.match(/username=.+(?=&usercookies)/g)[0].substr(9);
+  $('#loginNow').text(username);
+}
+
+function addToCheckerList(){
+  console.log ('addToCheckerList');
+  var checkerList;
+  if (cookieNow != null) {
+    chrome.storage.sync.get("checkerList",function (item){
+      console.log (item);
+      checkerList = item;
+    });
+
+    
+  }
+
+}
+
 getUserNow();
+$('#add').click(function(){
+  addToCheckerList();
+});
