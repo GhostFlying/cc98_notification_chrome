fdd4c452b482bfc8a5f7e3e56c9c0fd24797c989
GhostFlying <ghostflying@gmail.com>
2014-04-09 11:35:53 +0800

    add the decode and bug fixed

--- options.js
+++ options.js
@@ -17,24 +17,43 @@ function getUserNow(){
 
 function setUserNow(cookie){
   username = cookie.value.match(/username=.+(?=&usercookies)/g)[0].substr(9);
-  $('#loginNow').text(username);
+  $('#loginNow').text(decodeURIComponent(username));
 }
 
 function addToCheckerList(){
   console.log ('addToCheckerList');
   var checkerList;
   if (cookieNow != null) {
-    chrome.storage.sync.get("checkerList",function (item){
-      console.log (item);
-      checkerList = item;
+    chrome.storage.sync.get("checkerList",function (item){      
+      if (isEmptyObject(item)){
+        console.log('empty.');
+        checkerList = new Array();
+      }
+      else {
+        console.log ('not empty');
+        checkerList = item.checkerList;
+      }
+      checkerList.push(cookieNow.value);
+      chrome.storage.sync.set({'checkerList':checkerList}, function(){
+      console.log(checkerList);
     });
-
-    
+    });
+   
   }
 
+}
+
+
+function isEmptyObject(obj) {
+  for (var name in obj){
+    return false;
+  }
+  return true;
 }
 
 getUserNow();
 $('#add').click(function(){
   addToCheckerList();
 });
+
+chrome.storage.sync.clear(function(){});
